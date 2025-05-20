6dae8d6d3ebe0c9439dc4d4f6d70415e7ea5f513
Daniel-bot-dev <armstrongdanielfinnest9@gmail.com>
2025-05-20 15:19:38 +0100

    fix: app name and dotenv setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from django.utils.timezone import timedelta
+import os
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i)wnaq03o&_pd*qk5#*ni4m)8att(37+r$1u=95^7fytlbpx9r'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/urls.py
+++ mysite/urls.py
@@ -40,7 +40,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('app/', include('app.urls')),
+    path('mysite/', include('mysite.urls')),
     path('accounts/', include('accounts.urls')),
     
 ]
